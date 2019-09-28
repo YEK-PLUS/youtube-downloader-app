40cbed2ea7f2227d91e868c7e506a50c8bbf3b72
Yunus Emre <yek.plus.plus@gmail.com>
2019-09-28 14:30:45 +0000

    youtube conventer step 1

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Youtube Downloader Web App' });
+    res.render('index', { title: 'Youtube Downloader' });
 });
 
 // convert to human readable format
@@ -15,31 +15,53 @@ function bytesToSize(bytes) {
    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
    return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
 };
-
+function urlChecker(string) {
+  const pattern = new RegExp('^(https?:\\/\\/)?'+
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+
+    '(\\#[-a-z\\d_]*)?$','i');
+  return !!pattern.test(string);
+}
 
 router.post('/video', function(req, res, next) {
     var url = req.body.url,
-        formats = [],
+        formats ={
+            audio : [],
+            video:[]
+
+        },
+
         pattern = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/;
 
     request.get(url, function (err, resp, body) {
         // check if it is valid url
-        if(pattern.test(resp.request.uri.href)) {
+        if(!err && pattern.test(req.body.url)) {
             ytdl.getInfo(url, ['--youtube-skip-dash-manifest'], function(err, info) {
                 if(err) return res.render('listvideo', {error: 'The link you provided either not a valid url or it is not acceptable'});
 
                 // push all video formats for download (skipping audio)
+                console.log(info.formats[0].ext)
                 info.formats.forEach(function(item) {
                     if(item.format_note !== 'DASH audio' && item.filesize) {
+                        item.isAudio = item.ext == 'm4a';
+                        item.isVideo = item.ext == 'mp4';
                         item.filesize = item.filesize ? bytesToSize(item.filesize): 'unknown';
-                        formats.push(item);
+                        if(item.isAudio){formats.audio.push(item);}
+                        if(item.isVideo){formats.video.push(item);}
                     }
                 });
                 res.render('listvideo', {meta: {id: info.id, formats: formats}});
             })
         }
         else {
-            res.render('listvideo', {error: 'The link you provided either not a valid url or it is not acceptable'});
+            if(urlChecker(req.body.url)){
+                res.render('listvideo', {error: 'The link you provided either not a valid url or it is not acceptable'});
+            }
+            else{
+                res.redirect('/search/?query=' + req.body.url)
+            }
         }
     });
 
@@ -47,5 +69,16 @@ router.post('/video', function(req, res, next) {
 
 })
 
+router.all('/search', function(req, res, next) {
+
+    var url = req.query.query,
+        formats = [];
+
+    console.log(url)
+    res.render('index',{ title: 'Youtube Downloader' });
+
+
+
+});
 
 module.exports = router;
